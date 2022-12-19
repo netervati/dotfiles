92bb42092b200ead62a1f7af0af28e5a7fccd421
netervati <netervati@gmail.com>
2022-12-19 20:14:48 +0800

    Add bashrc folder for custom configs

--- .bashrc
+++ .bashrc
@@ -116,13 +116,8 @@ if ! shopt -oq posix; then
   fi
 fi
 
-export PATH="/home/netervati/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+# =====================
+# Custom configurations
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-eval "$(starship init bash)"
+source ~/bashrc/loader.sh
 
---
+++ bashrc/loader.sh
@@ -0,0 +1,18 @@
+# Loads installed dev dependencies
+
+
+# Init: Simple Python Version Management
+export PATH="/home/netervati/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+
+# Init: Node Version Manager
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+
+# Init: Starship: Cross-shell Prompt
+eval "$(starship init bash)"
+
