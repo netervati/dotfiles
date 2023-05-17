fa2e1bfa737949129ebc67d4a6cd74a93c67752c
netervati <netervati@gmail.com>
2023-05-17 21:08:23 +0800

    Add CHADtree config

---
+++ .config/nvim/after/plugin/chadtree.lua
@@ -0,0 +1,10 @@
+-- CHADTree settings
+
+
+local chadtree_settings = {
+  ["theme.text_colour_set"] = "solarized_universal",
+  ["view.width"] = 45
+}
+
+
+vim.api.nvim_set_var("chadtree_settings", chadtree_settings)
